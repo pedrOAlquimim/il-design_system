050b7a7eb3f64afa512eab8b13370d67edbb8c76
Pedro Alquimim <pedrohalquimim@gmail.com>
2022-10-14 08:37:41 -0300

    slot from radix-ui

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -23,3 +23,12 @@ export const Large: StoryObj<TextProps> = {
     size: 'lg',
   }
 }
+
+export const CustomComponent: StoryObj<TextProps> = {
+  args: {
+    asChild: true,
+    children: (
+      <p>Testando</p>
+    )
+  }
+}
