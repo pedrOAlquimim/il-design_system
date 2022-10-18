ea6800f29d0d83dc40f529ef7c62b3f5e30cb830
Pedro Alquimim <pedrohalquimim@gmail.com>
2022-10-17 23:41:50 -0300

    input component with focus

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -7,7 +7,7 @@ export function TextInput(props: TextInputProps) {
   return (
     <input 
       className={clsx(
-        'py-4 px-3 rounded bg-gray-800 w-full text-gray-100 text-xs placeholder:text-gray-400',
+        'py-4 px-3 rounded bg-gray-800 w-full outline-none text-gray-100 text-xs placeholder:text-gray-400 focus:ring-2 ring-cyan-300',
       )}
       {...props}
     />
